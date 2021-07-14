c788429e247c02639c1d9cd6c144d290bd1f508d
vannakvy <vannakvy2020@gmail.com>
2021-07-14 16:53:13 +0700

    add apollo client

--- src/App.js
+++ src/App.js
@@ -29,13 +29,70 @@ import SubPeople from './component/people/subPeople';
 import Hospital from './page/hospital';
 import SubHospital from './component/hospital/subHospital';
 import Login from './page/login'
+import {cache} from './cache'
+ 
+import {
+  split,
+   HttpLink,
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider
+} from "@apollo/client";
+import { createUploadLink } from 'apollo-upload-client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { WebSocketLink } from '@apollo/client/link/ws';
+
+
+ const upLoadLink = createUploadLink({
+    uri: "http://192.168.1.152:7000/graphql",
+    // headers:{
+    //   Authorization: localStorage.getItem('token')
+    // }
+  })
+
+
+// const httpLink = new HttpLink({
+//   uri: 'http://192.168.1.152:7000/graphql',
+
+// });
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://192.168.1.152:7000/graphql',
+  options: {
+    reconnect: true
+  }
+});
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  upLoadLink
+
+  
+);
+
+const client = new ApolloClient({
+  cache,
+  onError: ({ networkError, graphQLErrors }) => {
+    console.log('graphQLErrors', graphQLErrors)
+    console.log('networkError', networkError)
+  },
+
+  link:splitLink
+});
 
 const { Footer, Content } = Layout
 
 function App() {
-  const [login, setLogin] = useState(false)
+  const [login, setLogin] = useState(true)
 
   return (
+    <ApolloProvider client={client}>
     <Router>
       {login ? (
         <div className="App">
@@ -108,6 +165,7 @@ function App() {
       )}
 
     </Router>
+    </ApolloProvider>
   );
 }
 
